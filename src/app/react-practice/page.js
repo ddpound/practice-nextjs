a4d4544c519b4482df68ec5de0a118f04fcfea30
YouSeongJung <dpound@naver.com>
2023-12-19 17:27:26 +0900

    디벨롭 리액트 14번 항목 제거하기까지 완료

--- src/app/react-practice/page.js
+++ src/app/react-practice/page.js
@@ -1,23 +1,87 @@
-import Hello from "../components/Hello";
+'use client'
+
 import Wrapper from "../components/Wrapper";
+import { useState, useRef } from "react";
+import UserList from "../components/UserList";
+import CreateUser from "../components/CreateUser";
+
+function User({ user }) {
+    return (
+      <div>
+        <b>{user.username}</b> <span>({user.email})</span>
+      </div>
+    );
+  }
+
 
 export default function reactPractice(){
-    const name = "네임 아무거나 입력";
+    const [inputs, setInputs] = useState({
+        username: '',
+        email: ''
+      });
+
+    const { username, email } = inputs;
+    const onChange = e => {
+        const { name, value } = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value
+        });
+    };
+   
     
     const style = {
-        backgroundColor: 'black',
-        color: 'aqua',
+        color: `black`,
+        height: 1200,
         fontSize: 24, // 기본 단위 px
         padding: '1rem' // 다른 단위 사용 시 문자열로 설정
       }
 
+      const [users, setUsers] = useState([
+        {
+          id: 1,
+          username: 'velopert',
+          email: 'public.velopert@gmail.com'
+        },
+        {
+          id: 2,
+          username: 'tester',
+          email: 'tester@example.com'
+        },
+        {
+          id: 3,
+          username: 'liz',
+          email: 'liz@example.com'
+        }
+      ]);
+
+      const nextId = useRef(4);
+      const onCreate = () => {
+        // 나중에 구현 할 배열에 항목 추가하는 로직
+        const user = {
+            id: nextId.current,
+            username,
+            email
+          };
+        setUsers([...users, user]);
+        setInputs({
+            username: '',
+            email: ''
+          });
+
+        console.log(nextId.current)
+        nextId.current += 1;
+      };
+
+      const onRemove = id => {
+        // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+        // = user.id 가 id 인 것을 제거함
+        setUsers(users.filter(user => user.id !== id));
+      };
+
     return (
-        <>
+        <div >
             <div>리액트 연습페이지 입니다.</div>
-            <Hello name={name}/>
-            <Hello name={"유성중"}/>
-            <Hello name={"스타일 넣어봄"} style={style}/>
-            <Hello/>
 
             <hr/>
             
@@ -26,6 +90,23 @@ export default function reactPractice(){
             <Wrapper>
                 <div>보이나요?</div>
             </Wrapper>
-        </>
+
+            <hr/>
+            
+            <div style={style}>
+
+                <hr/>
+                <h4>배열 렌더링</h4>
+                
+                <CreateUser 
+                    username={username}
+                    email={email}
+                    onChange={onChange}
+                    onCreate={onCreate}
+                />
+                <UserList users={users} onRemove = {onRemove} />
+            </div>
+            <button>포커스 체크를 위한 초기화</button>
+        </div>
     )
 }
